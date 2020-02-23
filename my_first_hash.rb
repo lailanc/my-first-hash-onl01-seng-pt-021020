142edc8c839860891c3a2e9162870433cdeeb01c
Laila Campbell <lailacampbell96@gmail.com>
2020-02-23 18:12:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,8 +38,9 @@ def adding
                        #"gun powder" => 4
                        #})
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = "2"
-shipping_manifest["gun powder"] = "4"
+ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 
 
   # add 4 gun powder to the shipping_manifest hash below
